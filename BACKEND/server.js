5acdde6465f57cc227017f50549a1eb127f5b933
adityaa-nikam <adiityaa.nikam@gmail.com>
2026-08-10 17:00:07 +0530

    env frint

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -20,7 +20,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Home Route
